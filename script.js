365e053cbf29e060bdf22de21cfefd2c1fde79ff
Laiba <laibasajid4444@gmail.com>
2023-01-31 00:43:14 +0500

    i have added temprature convertor

---
+++ script.js
@@ -0,0 +1,34 @@
+    const calculateTemp = () => {
+
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+    // console.log(valueTemp);
+
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round((cel * 9 / 5) + 32);
+        return fahrenheit;
+    }
+
+    const fahTocel = (fehr) => {
+        let celsius = Math.round((fehr - 32) * 5 / 9);
+        return celsius;
+    }
+
+    let result;
+    if (valueTemp == 'cel') {
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = ` = ${result}°Fahrenheit`;
+    }
+    else {
+        result = fahTocel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = ` = ${result}°Celsius`;
+
+    }
+
+}
+
+
